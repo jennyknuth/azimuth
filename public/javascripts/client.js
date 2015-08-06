0393f058e93c9ebca9732d570c54b26cf688d482
Shannon Forsman <sforsman1@gmail.com>
2015-08-06 13:35:29 -0600

    got uv index to show up on page

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,16 +1,15 @@
-// var zip = document.getElementById('zip')
-// var button = document.getElementById('button')
-//
-//
-// button.addEventListener('click', function(e) {
-//   e.preventDefault()
-//   axios.post('/UV', {
-//       button: button.value
-//     })
-//     .then(function (response) {
-//       console.log(response);
-//     })
-//     .catch(function (response) {
-//       console.log(response);
-//   });
-// })
+var zip = document.getElementById('zip')
+var button = document.getElementById('button')
+var uv = document.getElementById('uv')
+
+
+button.addEventListener('click', function(e) {
+  e.preventDefault()
+  axios.post('/UV', {
+      button: button.value
+    })
+    .then(function (response) {
+      console.log(response)
+      uv.innerHTML = response.data[0].UV_VALUE
+    })
+})
--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST */
-router.post('/', function (req, res, next) {
+router.post('/UV', function (req, res, next) {
 console.log("req", req.body.zip);
-<<<<<<< HEAD
-=======
 
-unirest.get('http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/80306/JSON')
-.end(function (response) {
-  var UV_Value = response.body;
-  res.render('locations', {data: UV_Value});
-  console.log(UV_Value);
-  res.json(UV_Value)
-  res.redirect('/')
-})
-})
->>>>>>> 33584636a1330327c6ad7f9bfd991727ebc47104
-
- res.redirect('/')
-
+  unirest.get('http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/80306/JSON')
+  .end(function (response) {
+    var UV_Value = response.body;
+    res.render('locations', {data: UV_Value});
+    console.log(UV_Value);
+    res.json(UV_Value)
+  })
 })
 
 
