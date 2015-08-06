2a3c59a1c0fd769b0fdf7bebf2efebde6d293d16
Shannon Forsman <sforsman1@gmail.com>
2015-08-06 11:50:32 -0600

    added axios post request to /UV

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,1 +1,11 @@
-alert('hi')
+var zip = document.getElementById('zip')
+var button = document.getElementById('button')
+
+
+button.addEventListener('click', function(e) {
+  e.preventdefault()
+  axios.post('/UV', {
+      button: button.value
+      console.log(response)
+  })
+})
