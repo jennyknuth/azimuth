744298336165cb67084ce7e84bccef110c40d2eb
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-06 11:00:42 -0600

    added routes to views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cool UV Name' });
 });
 
+
+/* POST */
+router.post('/', function (req, res, next) {
+  res.redirect('index')
+})
 module.exports = router;
