a3bedb58c396832d87e277289e47a974c228c108
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-06 11:44:06 -0600

    locations route accesses EPA API

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,6 @@ router.get('/', function(req, res, next) {
 
 /* POST */
 router.post('/', function (req, res, next) {
-  //when we send a post request, it should look like this URL:
-  // http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/80306/JSON
   res.redirect('index')
 })
 
--- routes/locations.js
+++ routes/locations.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var unirest = require('unirest');
 
 /* GET locations listing. */
 router.get('/', function(req, res, next) {
-  res.send('locations');
+  unirest.get('http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVDAILY/ZIP/81212/JSON ')
+  .end(function (response) {
+    var UV_Value = response.body;
+    res.render('locations', {data: UV_Value});
+    console.log(UV_Value);
+  })
+  res.render('locations');
 });
+
 
 module.exports = router;
