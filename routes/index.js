bb20bd8eea1d3941773f5a3df27fb2f67f123a60
Shannon Forsman <sforsman1@gmail.com>
2015-08-07 09:58:57 -0600

    fixed morning time bug

--- routes/index.js
+++ routes/index.js
@@ -25,20 +25,26 @@ router.get('/', function(req, res, next) {
 router.post('/UV', function (req, res, next) {
   unirest.get('http://iaspub.epa.gov/enviro/efservice/getEnvirofactsUVHOURLY/ZIP/' + req.body.zip + '/JSON')
   .end(function (response) {
-    
     var time = new Date
     var hour = time.getHours();
     var epaApi = response.body // an array
-    var UV_Value = 0
+    var UV_Value = 0;
     epaApi.forEach(function(block) {
+
       var epaTime = block.DATE_TIME
       epaTime = epaTime.split(' ')
       epaHour = epaTime[1]
+
+      if (epaHour[0] === '0') {
+        epaHour = epaHour[1]
+        epaHour = parseInt(epaHour)
+      }
       if (epaTime[2] === "PM") {
         epaHour = parseInt(epaHour) + 12
       }
       if (epaHour === hour) {
         UV_Value = block.UV_VALUE
+        console.log('hi', UV_Value)
       }
     })
     unirest.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.body.zip + '&components=postal_code&key=' + process.env.GOOGLE_KEY)
