5e4414af3d49b6b5292c9f6c59cd5eb113c29847
Jenny Knuth <inkboco@gmail.com>
2018-04-07 14:57:37 -0600

    add favicon

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res, next) {
 router.post('/UV', function (req, res, next) {
   var lat, long;
 
-  axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.body.zip + '&components=postal_code&key=' + process.env.GOOGLE_KEY).then(function (response) {
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.body.zip + '&components=postal_code&key=' + process.env.GOOGLE_KEY)
+  .then(function (response) {
     lat = response.data.results[0].geometry.location.lat.toFixed(2);
     long = response.data.results[0].geometry.location.lng.toFixed(2);
     return axios.get('http://api.openweathermap.org/data/2.5/uvi?appid='+ process.env.OWM_KEY +'&lat=' + lat + '&lon=' + long);
